abdcc7e41673958bf3331a4f54a698de2b3c04b4
Caleb16 <crank1@swceschool.org>
2015-02-02 20:55:55 -0700

    Added Files

---
+++ Unique-Random-Numbers/Unique-Random-Numbers/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 9!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(49, 67)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(213, 39)
+        Me.btnCalculate.TabIndex = 0
+        Me.btnCalculate.Text = "Click to Create Number"
+        Me.btnCalculate.UseVisualStyleBackColor = true
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(283, 22)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Click to Create a Random Number"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = true
+        Me.lblResult.Location = New System.Drawing.Point(112, 129)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 13)
+        Me.lblResult.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(317, 171)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Name = "Form1"
+        Me.Text = "Unique Random Numbers"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+
+End Class
---
+++ Unique-Random-Numbers/Unique-Random-Numbers/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim intNumber1 As Integer
+        Dim intNumber2 As Integer
+        Dim IntNumber3 As Integer
+                 
+        Randomize()
+        intNumber1 = Int((9 - 1 + 1) * Rnd() + 1)
+
+        Do
+            Randomize()
+            intNumber2 = Int((9 - 1 + 1) * Rnd() + 1)
+        Loop Until intNumber2 <> intNumber1
+        Do
+            Randomize()
+            intNumber3 = Int((9 - 1 + 1) * Rnd() + 1)
+        Loop Until intNumber3 <> intNumber2 And intNumber3 <> intNumber2
+
+        If (intNumber1 <> intNumber2) and (intNumber1 <> intNumber3) and (intNumber2 <> intNumber3)
+            Me.lblResult.Text = "" & intNumber1 & intNumber2 & intNumber3 & ""
+        End If
+    End Sub
+End Class
